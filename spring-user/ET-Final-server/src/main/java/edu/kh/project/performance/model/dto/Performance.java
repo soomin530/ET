4bb0ed4164cd3b51df4a5a29daf489b5281c65e3
soomin530 <rnldudnstnalsdl@gmail.com>
2024-12-27 11:15:10 +0900

    공연장 위도 경도 조회

--- spring-user/ET-Final-server/src/main/java/edu/kh/project/performance/model/dto/Performance.java
+++ spring-user/ET-Final-server/src/main/java/edu/kh/project/performance/model/dto/Performance.java
@@ -29,6 +29,9 @@ public class Performance {
 	private String prfstate;		// 공연상태
 	private String mt10id;			// 공연시설
 	private String styurl;			// 소개이미지
+	private String fcltla;			// 위도
+	private String fcltlo;			// 경도
+	
 	
 	
 
