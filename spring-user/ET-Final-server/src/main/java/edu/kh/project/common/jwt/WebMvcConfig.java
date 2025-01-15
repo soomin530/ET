7380d2afa4ab73bbd5119156e8bdefbce86f66dd
Jung-Dae_seung <eotmd1213@naver.com>
2025-01-15 17:24:57 +0900

    일부 수정

--- spring-user/ET-Final-server/src/main/java/edu/kh/project/common/jwt/WebMvcConfig.java
+++ spring-user/ET-Final-server/src/main/java/edu/kh/project/common/jwt/WebMvcConfig.java
@@ -1,11 +1,15 @@
 package edu.kh.project.common.jwt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -18,8 +22,25 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Value("${my.performance.resource-location}")
     private String resourceLocation;
     
+    @Autowired
+    private Environment env;
+    
+    @PostConstruct
+    public void checkProperties() {
+        System.out.println("=== Resource Properties in WebMvcConfig ===");
+        System.out.println("From @Value resourceHandler: " + resourceHandler);
+        System.out.println("From @Value resourceLocation: " + resourceLocation);
+        System.out.println("From Environment resourceHandler: " + 
+            env.getProperty("my.performance.resource-handler"));
+        System.out.println("From Environment resourceLocation: " + 
+            env.getProperty("my.performance.resource-location"));
+    }
+    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	System.out.println("Resource Handler: " + resourceHandler);
+        System.out.println("Resource Location: " + resourceLocation);
+        
         registry.addResourceHandler(resourceHandler)
                 .addResourceLocations(resourceLocation)
                 .setCachePeriod(3600)
