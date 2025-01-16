b1ef35939d8db0551c09d77ed7bf026b2bf2d2da
Jung-Dae_seung <eotmd1213@naver.com>
2025-01-16 11:57:52 +0900

    임시 출력

--- react/et-admin-client/src/api/axoisAPI.js
+++ react/et-admin-client/src/api/axoisAPI.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 export const axiosApi = axios.create({
-  baseURL : 'http://localhost:8081',
+  baseURL : 'http://43.202.85.129:8081',
   headers : {'Content-Type' : 'application/json'},
   credentials: 'include'
   // withCredentials : true // 쿠키 포함 설정
