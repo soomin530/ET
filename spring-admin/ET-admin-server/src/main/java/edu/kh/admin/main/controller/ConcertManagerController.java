7f119bb90e20216fa00cca9883375f429b3883a0
Jung-Dae_seung <eotmd1213@naver.com>
2025-01-16 11:13:18 +0900

    어드민 주소

--- spring-admin/ET-admin-server/src/main/java/edu/kh/admin/main/controller/ConcertManagerController.java
+++ spring-admin/ET-admin-server/src/main/java/edu/kh/admin/main/controller/ConcertManagerController.java
@@ -20,7 +20,7 @@ import edu.kh.admin.main.model.service.ConcertManagerService;
 import lombok.RequiredArgsConstructor;
 
 @RestController 
-@CrossOrigin( origins = "http://localhost:3000",
+@CrossOrigin( origins = "https://final-project-react-individual.vercel.app",
 			  allowedHeaders = "*",
 			  allowCredentials = "true",
 			  methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, 
